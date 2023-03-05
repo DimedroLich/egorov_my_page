f762c1a0ade642bbd3af17a024bd5a34f037e992
DimedroLich <1DimedroL1@gmail.com>
2023-03-05 16:33:46 +0300

    Added dinamic URL for horoscope_signs. Bunch of different functions from horoscope view replaced with one main func

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -1,17 +1,6 @@
 from django.urls import path
 import views
 urlpatterns = [
-
-    path('leo/', views.leo),
-    path('scorp/', views.scorpio),
-    path('aries/', views.aries),
-    path('taurus/', views.taurus),
-    path('gemini/', views.gemini),
-    path('cancer/', views.cancer),
-    path('virgo/', views.virgo),
-    path('libra/', views.libra),
-    path('sagittarius/', views.sagittarius),
-    path('capricorn/', views.capricorn),
-    path('aquarius/', views.aquarius),
-    path('pisces/', views.pisces),
+    path('',views.all_horoscope),
+    path('<sign_zodiac>',views.get_info_about_zodiac),
 ]
--- horoscope/views.py
+++ horoscope/views.py
@@ -1,55 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 
 # Create your views here.
 def all_horoscope(request):
     return HttpResponse('<h1>Страница со всеми знаками зодиака</h1>')
 
-
-def leo(request):
-    return HttpResponse('Страница знака зодика Лев')
-
-
-def scorpio(request):
-    return HttpResponse('Страница знака зодика Скорпион')
-
-
-def aries(request):
-    return HttpResponse('Страница знака зодика Овен')
-
-
-def taurus(request):
-    return HttpResponse('Страница знака зодика Телец')
-
-
-def gemini(request):
-    return HttpResponse('Страница знака зодика Близнецы')
-
-
-def cancer(request):
-    return HttpResponse('Страница знака зодика Рак')
+def get_info_about_zodiac(request,sign_zodiac:str):
+    signs = {
+        "aries": "Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля).",
+        "taurus": "Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая).",
+        "gemini": "Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня).",
+        "cancer": "Рак - четвёртый знак зодиака, Луна (с 22 июня по 22 июля).",
+        "leo": "Лев - пятый знак зодиака, солнце (с 23 июля по 21 августа).",
+        "virgo": "Дева - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября).",
+        "libra": "Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября).",
+        "scorpio": "Скорпион - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября).",
+        "sagittarius": "Стрелец - девятый знак зодиака, планета Юпитер (с 23 ноября по 22 декабря).",
+        "capricorn": "Козерог - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января).",
+        "aquarius": "Водолей - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля).",
+        "pisces": "Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта)."
+    }
+    if sign_zodiac.lower() in signs:
+        return HttpResponse(signs[sign_zodiac.lower()])
+    else:
+        raise Http404()
 
 
-def virgo(request):
-    return HttpResponse('Страница знака зодика Дева')
-
-
-def libra(request):
-    return HttpResponse('Страница знака зодика Весы')
-
-
-def sagittarius(request):
-    return HttpResponse('Страница знака зодика Стрелец')
-
-
-def capricorn(request):
-    return HttpResponse('Страница знака зодика Козерог')
-
-
-def aquarius(request):
-    return HttpResponse('Страница знака зодика Водолей')
-
-
-def pisces(request):
-    return HttpResponse('Страница знака зодика Рыбы')
