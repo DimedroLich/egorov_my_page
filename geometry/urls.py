312d7dbbefe2fe6c919eccbf7abdf7060f9e767e
DimedroLich <1DimedroL1@gmail.com>
2023-03-07 14:50:23 +0300

    Geometry app: Added redirect

--- geometry/urls.py
+++ geometry/urls.py
@@ -4,4 +4,7 @@ urlpatterns = [
     path('rectangle/<int:width>/<int:height>',views.rect),
     path("square/<int:width>",views.square),
     path('circle/<int:radius>',views.circle),
+    path("get_rectangle_area/<int:width>/<int:height>",views.get_rectangle_area),
+    path("get_square_area/<int:width>",views.get_square_area),
+    path("get_circle_area/<int:radius>",views.get_circle_area),
 ]
--- geometry/views.py
+++ geometry/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from math import pi
+
+
 # Create your views here.
-def rect(request,width,height):
-    return HttpResponse(f'Площадь прямоугольника размером {width}x{height} равна {width*height}')
+def rect(request, width, height):
+    return HttpResponse(f'Площадь прямоугольника размером {width}x{height} равна {width * height}')
 
-def square(request,width:int):
-    return HttpResponse(f'<h2>Площадь квадрата размером {width}x{width} равна {width**2}')
 
-def circle(request,radius:int):
-    return HttpResponse(f'<h2>Площадь круга радиусом {radius} равна {round(pi * radius ** 2,2)}')
+def square(request, width: int):
+    return HttpResponse(f'<h2>Площадь квадрата размером {width}x{width} равна {width ** 2}')
+
+
+def circle(request, radius: int):
+    return HttpResponse(f'<h2>Площадь круга радиусом {radius} равна {round(pi * radius ** 2, 2)}')
+
+
+def get_rectangle_area(request, width, height):
+    return HttpResponseRedirect(f'/calculate_geometry/rectangle/{width}/{height}')
+
+def get_square_area(request, width: int):
+    return HttpResponseRedirect(f'/calculate_geometry/square/{width}')
+
+def get_circle_area(request, radius: int):
+    return HttpResponseRedirect(f'/calculate_geometry/circle/{radius}')
