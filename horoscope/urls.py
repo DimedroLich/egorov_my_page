25ca3ce00ba795cd3108c799b4d0911def58d65a
DimedroLich <1DimedroL1@gmail.com>
2023-03-10 12:52:23 +0300

    Horoscope app: Added url for elements and redirect from elements to specific sign

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 import views
 urlpatterns = [
     path('',views.index),
+    path('type/', views.zodiac_types),
+    path('type/<element>', views.signs_by_element,name = 'element'),
     path('<int:sign_zodiac>/',views.get_info_about_zodiac_by_num),
     path('<str:sign_zodiac>/',views.get_info_about_zodiac,name = 'horoscop-name'),
+
     ]
--- horoscope/views.py
+++ horoscope/views.py
@@ -1,30 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404,HttpResponseRedirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
 
 # Create your views here.
 
 signs = {
-        "aries": "Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля).",
-        "taurus": "Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая).",
-        "gemini": "Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня).",
-        "cancer": "Рак - четвёртый знак зодиака, Луна (с 22 июня по 22 июля).",
-        "leo": "Лев - пятый знак зодиака, солнце (с 23 июля по 21 августа).",
-        "virgo": "Дева - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября).",
-        "libra": "Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября).",
-        "scorpio": "Скорпион - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября).",
-        "sagittarius": "Стрелец - девятый знак зодиака, планета Юпитер (с 23 ноября по 22 декабря).",
-        "capricorn": "Козерог - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января).",
-        "aquarius": "Водолей - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля).",
-        "pisces": "Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта)."
-    }
+    "aries": "Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля).",
+    "taurus": "Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая).",
+    "gemini": "Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня).",
+    "cancer": "Рак - четвёртый знак зодиака, Луна (с 22 июня по 22 июля).",
+    "leo": "Лев - пятый знак зодиака, солнце (с 23 июля по 21 августа).",
+    "virgo": "Дева - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября).",
+    "libra": "Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября).",
+    "scorpio": "Скорпион - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября).",
+    "sagittarius": "Стрелец - девятый знак зодиака, планета Юпитер (с 23 ноября по 22 декабря).",
+    "capricorn": "Козерог - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января).",
+    "aquarius": "Водолей - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля).",
+    "pisces": "Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта)."
+}
+
+elements = {
+    "fire": ('Огненные знаки','aries', 'leo', 'sagittarius'),
+    "earth": ('Знаки земли',"taurus", "virgo", "capricorn"),
+    "air": ('Воздушные знаки',"gemini", "libra", "aquarius"),
+    "water": ('Водные знаки',"cancer", "scorpio", "pisces"),
+}
+
 
 def index(request):
     signs_ = list(signs)
     signs_template = ""
     for s in signs_:
         url_path = reverse("horoscop-name", args=(s,))
-        signs_template+=f'<li><a href={url_path}>{s}</a></li>'
+        signs_template += f'<li><a href={url_path}>{s}</a></li>'
     template = f"""
                 <h3>Знаки зодиака</h3>
                 <ul>
@@ -32,18 +40,49 @@ def index(request):
                 </ul>
                 """
     return HttpResponse(template)
-def get_info_about_zodiac(request,sign_zodiac:str):
 
+
+def get_info_about_zodiac(request, sign_zodiac: str):
     if sign_zodiac.lower() in signs:
         return HttpResponse(signs[sign_zodiac.lower()])
     else:
         raise Http404(f'Неизвестный знак зодиака {sign_zodiac}')
 
 
-def get_info_about_zodiac_by_num(request,sign_zodiac:int):
+def get_info_about_zodiac_by_num(request, sign_zodiac: int):
     if not len(signs) >= int(sign_zodiac) >= 1:
         raise Http404(f'Неизвестный знак зодиака {sign_zodiac}')
     signs_ = list(signs)
-    current_sign = signs_[sign_zodiac-1]
-    redirect_url = reverse("horoscop-name",args= (current_sign,))
+    current_sign = signs_[sign_zodiac - 1]
+    redirect_url = reverse("horoscop-name", args=(current_sign,))
     return HttpResponseRedirect(redirect_url)
+
+
+def zodiac_types(request):
+    all_elem = list(elements)
+    elems = ''
+    for i in all_elem:
+        url = reverse('element',args=(i,))
+        elems += f'<li><a href={url}>{i.title()}</a></li>'
+    response = f'''
+                <h3>Стихии знаков зодиака</h3>
+                <ul>
+                    {elems}
+                </ul>
+                '''
+    return HttpResponse(response)
+
+def signs_by_element(request, element):
+    if not element in elements:
+        raise Http404()
+    signs = ''
+    for elem in elements[element][1:]:
+        url = reverse('horoscop-name',args=(elem,))
+        signs += f'<li><a href={url}>{elem.title()}</a></li>'
+    response = f'''
+            <h2>{elements[element][0]}</h2>
+            <ul>
+                {signs}
+            </ul>
+            '''
+    return HttpResponse(response)
