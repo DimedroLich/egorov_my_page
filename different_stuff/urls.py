09c11f87e2859b298c099bbeb66eb0c56d1af007
DimedroLich <1DimedroL1@gmail.com>
2023-03-15 17:33:45 +0300

    Added html templates for diff/people; diff/people_detail. With 'for' tag in it

--- different_stuff/urls.py
+++ different_stuff/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('kianu/',views.kianu),
-    path('guiness/',views.get_guinness_world_records)
+    path('guiness/',views.get_guinness_world_records),
+    path('people/',views.people),
+    path('people_detail/',views.people_detail),
 ]
